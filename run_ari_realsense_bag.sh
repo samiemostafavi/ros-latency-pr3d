4ac4aaafaa254caa54104d2b9c6718a12bc1f757
Samie Mostafavi <samiemostafavi@gmail.com>
2024-06-13 08:48:01 +0200

    Update run_ari_realsense_bag.sh

--- run_ari_realsense_bag.sh
+++ run_ari_realsense_bag.sh
@@ -2,7 +2,7 @@
 source /root/catkin_ws/devel/setup.bash
 
 #configure ROS
-ari_slave <edge IP>
+ros_slave <edge IP>
 
 #play rosbag looping in background with no stdout or stderr
 rosbag play -l </path/to/bag> --topics /head_front_camera/depth/color/points > /dev/null 2>&1 &
